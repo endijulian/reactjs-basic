e5520f532ee85ddcbc90f855538ca7845ff04188
endijulian <endijulian080798@gmail.com>
2023-06-12 15:20:20 +0700

    variabel

---
+++ src/Variabel.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+//Const
+// const harga = 2000;
+
+//var
+// var harga = 50000;
+// if (true) {
+//     var harga = 30000;
+// }
+
+let harga = 50000;
+if (true) {
+    let harga = 30000;
+}
+
+ const Variabel = () => {
+  return (
+    <div>
+        <h2>Harga : {harga}</h2>
+    </div>
+  )
+}
+
+export default Variabel
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+import Variabel from './Variabel';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Variabel></Variabel>
   </React.StrictMode>
 );
